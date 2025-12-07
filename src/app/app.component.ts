186c16ebfaa4636ce772e6bfb47af65a2d60da96
shayne-lwl <shayne.weiliang@gmail.com>
2025-12-07 15:00:54 +0800

    chore: initialize Angular project
    - Generated new Angular 19 project
    - Installed @angular/material
    - Set up base configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  template: '<h1>Hello World</h1>',
 })
-export class AppComponent {
-  title = 'wheretopark';
-}
+export class AppComponent {}
